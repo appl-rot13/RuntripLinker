2b818a4c268b4c99d30fb83c7b2d26e393dc8a65
rot <appl.rot13@gmail.com>
2024-06-19 00:11:04 +0900

    Improve to continue the main loop on exception

--- index.php
+++ index.php
@@ -6,20 +6,26 @@ $userId = $_ENV['RUNTRIP_USER_ID'];
 $journalId = getLatestJournal($userId)['id'];
 
 while (true) {
-    $journals = getNewJournals($userId, $journalId);
-    foreach ($journals as $journal) {
-        $journalId = $journal['id'];
+    try {
+        $journals = getNewJournals($userId, $journalId);
+        foreach ($journals as $journal) {
+            $journalId = $journal['id'];
 
-        $tags = implode(' ', array_filter(array_map('sanitizeHashtag', $journal['tags'])));
-        $journalUrl = 'https://runtrip.jp/journals/' . $journalId;
-        $text = createTweetText($journal['description'], $tags, $journalUrl);
-        if ($text === '') {
-            continue;
+            $tags = implode(' ', array_filter(array_map('sanitizeHashtag', $journal['tags'])));
+            $journalUrl = 'https://runtrip.jp/journals/' . $journalId;
+            $text = createTweetText($journal['description'], $tags, $journalUrl);
+            if ($text === '') {
+                continue;
+            }
+
+            $imageUrl = $journal['imageUrls'][0];
+            $result = tweet($text, $imageUrl, $_ENV['TWITTER_ACCESS_TOKEN'], $_ENV['TWITTER_ACCESS_TOKEN_SECRET']);
+            logging($result);
         }
-
-        $imageUrl = $journal['imageUrls'][0];
-        $result = tweet($text, $imageUrl, $_ENV['TWITTER_ACCESS_TOKEN'], $_ENV['TWITTER_ACCESS_TOKEN_SECRET']);
-        logging($result);
+    } catch (Throwable $t) {
+        logging($t);
+    } catch (Exception $e) {
+        logging($e);
     }
 
     sleep($_ENV['CHECK_INTERVAL']);
